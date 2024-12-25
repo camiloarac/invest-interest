d096a73167fc8ae2a4ec8e1897289e83feaf40df
Camilo Andres Rey Arias <camiloarac@gmail.com>
2024-12-25 22:19:24 +0100

    Add labels and format to the plot

--- plot.py
+++ plot.py
@@ -5,5 +5,9 @@ def plot_xy(x: list, y: list):
     x_arr = np.array(x)
     y_arr = np.array(y)
     fig, ax = plt.subplots()
-    ax.scatter(x_arr, y_arr)
+    ax.plot(x_arr, y_arr, color="blue", linewidth=1)
+    ax.set_xlabel("Years")
+    ax.set_ylabel("Savings")
+    ax.set_title("Savings over the years")
+
     plt.show()
